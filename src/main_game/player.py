cb795b88d5fa2526bbb70b6c746b0753536f00ea
ccmour <seymouc2@union.edu>
2017-02-22 17:48:23 -0500

    pushing so I can work on a different computer/save some typing. Added init methods for player and battle

--- src/main_game/player.py
+++ src/main_game/player.py
@@ -1,1 +1,13 @@
 # Player class, player objects can be NPCs or user controlled
+
+
+class Player:
+
+    def __init__(self, hp, energy, arm, str, dex, int, stam):
+        self.hp = hp + 10*stam
+        self.energy = energy
+        self.arm = arm
+        self.str = str
+        self.dex = dex
+        self.int = int
+        self.stam = stam
