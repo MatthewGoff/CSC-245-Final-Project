7ab33c287fa2b622675a760952651b093926a086
MonicaLiii <liy@union.edu>
2017-02-28 17:17:50 -0500

    updated animation

--- src/main_game/animation.py
+++ src/main_game/animation.py
@@ -4,67 +4,111 @@
 
 import pygame
 
+
 class PlayerAnimation(pygame.sprite.Sprite):
 
+    # The width and height of the player sprite sheet.
     _width = 576
     _height = 48
+    # The numbers of player sprites on the sprite sheet
     _number = 12
-    images = []
+    # The height and width of one player sprite is 48 pixels.
+    _length = 48
+    # Keeps all the moving sprites
+    mov_imgs = []
+    # Keeps all the standing/stop sprites
+    stp_imgs = []
 
     def __init__(self, x, y):
 
         pygame.sprite.Sprite.__init__(self)
 
+        # The position of the player, left-up corner
         self.x = x
         self.y = y
+        self.speed = 10
 
-        # loading the full player image as a surface, set the index to default zero
+        # Loads the full player image as a surface, sets the
+        # index to default zero
         self.full_img = pygame.image.load("player.png").convert_alpha()
         self.index = 0
 
-        if len(self.images) == 0:
-            # add each part of player action pic as a subsurface
-            # there are 12 separate parts
-            self.images = [self.full_img.subsurface(pygame.Rect( (i * 48,0),(48,48)))
+        # Adds each part of player action pic as a subsurface
+        if len(self.mov_imgs) == 0:
+            self.mov_imgs = [self.full_img.subsurface(pygame.Rect((i * self._length,0),(self._length,self._length)))
                          for i in xrange(self._number)]
 
-        # make a list of subsurface
-        self.image = self.images[self.index+1]
+        # Separates sprites into two list: one keeps all moving
+        # sprites, one keeps all standing/stop sprites.
+        self.stp_imgs = [self.mov_imgs[1], self.mov_imgs[4], self.mov_imgs[7], self.mov_imgs[10]]
+        for i in range(0, len(self.stp_imgs)):
+            self.mov_imgs.remove(self.stp_imgs[i])
+
+        # Standard requirements of Sprite obj for drawing
+        self.image = self.stp_imgs[self.index]
         self.rect = pygame.Rect(x, y, self._width, self._height)
 
+    # Getter for x
+    def get_x(self):
+        return self.rect.x
+
+    # Getter for y
+    def get_y(self):
+        return self.rect.y
+
+    # Setter for x
+    def set_x(self, x):
+        self.rect.x = x
+
+    # Setter for x
+    def set_y(self, y):
+        self.rect.y = y
+
+    # Makes the player walk by updating its position
+    def walk(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    # When pressing s, player moves down to the screen by steps
     def face_moving(self):
-        # index 0 to 2 are for face movement
-        self.index = 0
-        self.image = self.images[self.index]
+        self.index += 1
+        if self.index > 1:
+            self.index = 0
+        self.image = self.mov_imgs[self.index]
 
-        "Tried to switch pictures here, but failed."
-        #pygame.time.wait(100)
-        #self.index = 2
-        #self.image = self.images[self.index]
-
-    def left_moving(self):
-        # index 3 and 5 are for left movement
-        self.index = 3
-        self.image = self.images[self.index]
-
-    def right_moving(self):
-        # index 6 and 8 are for right movement
-        self.index = 6
-        self.image = self.images[self.index]
-
+    # When pressing w, player moves up to the screen by steps
     def back_moving(self):
-        # index 9 and 11 are for back movement
-        self.index = 9
-        self.image = self.images[self.index]
+        self.index += 1
+        if self.index > 7 or self.index < 6:
+            self.index = 6
+        self.image = self.mov_imgs[self.index]
 
+    # When pressing a, player moves left to the screen by steps
+    def left_moving(self):
+        self.index += 1
+        if self.index > 3 or self.index < 2:
+            self.index = 2
+        self.image = self.mov_imgs[self.index]
+
+    # When pressing d, player moves right to the screen by steps
+    def right_moving(self):
+        self.index += 1
+        if self.index > 5 or self.index < 4:
+            self.index = 4
+        self.image = self.mov_imgs[self.index]
+
+    # Sets the player to stop, facing to the screen
     def face_stop(self):
-        self.image = self.images[1]
+        self.image = self.stp_imgs[0]
 
-    def left_stop(self):
-        self.image = self.images[4]
-
-    def right_stop(self):
-        self.image = self.images[7]
-
+    # Sets the player to stop, turning back to the screen
     def back_stop(self):
-        self.image = self.images[10]
+        self.image = self.stp_imgs[3]
+
+    # Sets the player to stop, facing to the left
+    def left_stop(self):
+        self.image = self.stp_imgs[1]
+
+    # Sets the player to stop, facing to the right
+    def right_stop(self):
+        self.image = self.stp_imgs[2]
--- src/main_game/anime_test.py
+++ src/main_game/anime_test.py
@@ -13,6 +13,20 @@ class AnimeTest(object):
     my_win = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
     players = pygame.sprite.Group()
 
+    # Current frame - the current number of frame, also means how many
+    #           frames have passed.
+    current_frame = 0
+    # Animation frame - how many frames should passed before switching
+    #           to next animation
+    animation_frame = 5
+
+    # Instead of boolean, int is used here to determine which direction
+    # is the player moving to.
+    # 0 - stop
+    # 1 - 's', move down; 2 - 'w', move up
+    # 3 - 'a', move left; 4 - 'd', move right
+    direction = 0
+
     @classmethod
     def init(cls):
         pygame.init()
@@ -23,7 +37,9 @@ class AnimeTest(object):
     def handle_events(cls):
 
         keep_going = True
+
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 keep_going = False
 
@@ -33,16 +49,16 @@ class AnimeTest(object):
                 #print "Key pressed = %s" % key_pressed + "%d" % ord(key_pressed)
 
                 if key_pressed == "s":
-                    cls.player.face_moving()
+                    cls.direction = 1
 
                 elif key_pressed == "w":
-                    cls.player.back_moving()
+                    cls.direction = 2
 
                 elif key_pressed == "a":
-                    cls.player.left_moving()
+                    cls.direction = 3
 
                 elif key_pressed == "d":
-                    cls.player.right_moving()
+                    cls.direction = 4
 
             elif event.type == pygame.KEYUP:
 
@@ -50,24 +66,44 @@ class AnimeTest(object):
                 key_released = chr(event.dict['key'] % 256)
 
                 if key_released == "s":
+                    cls.direction = 0
                     cls.player.face_stop()
 
                 elif key_released == "w":
+                    cls.direction = 0
                     cls.player.back_stop()
 
                 elif key_released == "a":
+                    cls.direction = 0
                     cls.player.left_stop()
 
                 elif key_released == "d":
+                    cls.direction = 0
                     cls.player.right_stop()
 
         return keep_going
 
     @classmethod
+    def apply_animation(cls):
+        # Checks the direction, and call the animation function to update imgs
+
+            if cls.direction == 1:  # 's', move down
+                cls.player.face_moving()
+                cls.player.walk(0, cls.player.speed)
+            elif cls.direction == 2:  # 'w', move up
+                cls.player.back_moving()
+                cls.player.walk(0, -cls.player.speed)
+            elif cls.direction == 3:  # 'a', move left
+                cls.player.left_moving()
+                cls.player.walk(-cls.player.speed, 0)
+            elif cls.direction == 4:  # 'd', move right
+                cls.player.right_moving()
+                cls.player.walk(cls.player.speed, 0)
+
+    @classmethod
     def draw(cls):
         # Draw Background
         cls.my_win.fill(pygame.color.Color("LightGrey"))
-        #pygame.draw.rect(cls.my_win,(165,165,165),())
         cls.players.draw(cls.my_win)
         # Swap display
         pygame.display.update()
@@ -79,28 +115,28 @@ class AnimeTest(object):
     @classmethod
     def run(cls):
 
-        frame_rate = 240
+        frame_rate = 30
         tick_time = int(1.0 / frame_rate * 1000)
 
         # The game loop starts here.
         keep_going = True
         while keep_going:
 
-            #pygame.time.wait(tick_time)
+            # Keeps track of frames since last image was changed
+            cls.current_frame += 1
 
-            frame_rate = 240
-            tick_time = int(1.0 / frame_rate * 1000)
+            keep_going = cls.handle_events()
 
-            # The game loop starts here.
-            keep_going = True
-            while keep_going:
-                keep_going = cls.handle_events()
+            # Changes images when it's time to change
+            if cls.current_frame > cls.animation_frame:
+                cls.apply_animation()
+                # Resets tracking frame to zero again to restart
+                # a new checking progress
+                cls.current_frame = 0
 
-                pygame.time.wait(tick_time)
+            pygame.time.wait(tick_time)
 
-                cls.draw()
-
-            cls.quit()
+            cls.draw()
 
         cls.quit()
 
