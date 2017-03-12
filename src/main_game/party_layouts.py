7b4c9319c01e9b86d5df77224187ed015f0a49c8
MonicaLiii <liy@union.edu>
2017-03-12 18:14:19 -0400

    enemies pictures updated

--- src/main_game/party_layouts.py
+++ src/main_game/party_layouts.py
@@ -31,17 +31,17 @@ class PartyTracker:
 
 
     def olin107(self, campaign, battle_listener):
-        enemy_image = pygame.image.load("../assets/images/OtherSheet.png").convert_alpha()
-        enemy_rect = pygame.Rect(2016, 224, 32, 32)
+        enemy_image = pygame.image.load("../assets/images/zombie.png").convert_alpha()
+        enemy_rect = pygame.Rect(48, 0, 48, 48)
         enemy_image = enemy_image.subsurface(enemy_rect).copy()
         enemy_party = Party((90, 460),
-                      32,
-                      (32, 32),
+                      48,
+                      (48, 48),
                       enemy_image,
                       campaign.world,
                       battle_listener)
-        enemy1 = Player(550, 200, 68, 98, "../assets/images/OtherSheet.png", 2016, 224, 32, 32, False)
-        enemy2 = Player(550, 50, 68, 98, "../assets/images/OtherSheet.png", 2016, 224, 32, 32, False)
+        enemy1 = Player(550, 200, 72, 72, "../assets/images/zombie.png", 48*4, 0, 48, 48, False)
+        enemy2 = Player(550, 50, 72, 72, "../assets/images/zombie_magic.png", 48*5, 0, 48, 48, False)
         enemy_party.members += [enemy1, enemy2]
         campaign.enemy = enemy_party
         campaign.world.add_party(enemy_party)
@@ -50,44 +50,44 @@ class PartyTracker:
         player_rect = pygame.Rect(0, 0, 48, 48)
         player_image = player_image.subsurface(player_rect).copy()
         user = Party(campaign.world.get_border().center,
-                     32,
-                     (10, 10),
+                     48,
+                     (10, 25),
                      player_image,
                      campaign.world,
                      battle_listener)
         campaign.user = user
-        player = Player(20, 200, 68, 98, "../assets/images/player.png", 384, 0, 38, 48, True)
+        player = Player(20, 200, 72, 72, "../assets/images/player.png", 384, 0, 48, 48, True)
         player.abilities = [Energize(), Fireball(0, 0, 0), PowerAttack(), Heal()]
         player.change_hp(100)
         campaign.user.members += [player]
         campaign.world.add_party(user)
 
     def olinhallway(self, campaign, battle_listener):
-        enemy_image = pygame.image.load("../assets/images/OtherSheet.png").convert_alpha()
-        enemy_rect = pygame.Rect(384, 160, 32, 32)
+        enemy_image = pygame.image.load("../assets/images/zombie2.png").convert_alpha()
+        enemy_rect = pygame.Rect(48, 0, 48, 48)
         enemy_image = enemy_image.subsurface(enemy_rect).copy()
         enemy_party = Party((532, 540),
-                      32,
-                      (32, 32),
+                      48,
+                      (48, 48),
                       enemy_image,
                       campaign.world,
                       battle_listener)
-        enemy1 = Player(550, 200, 68, 98, "../assets/images/OtherSheet.png", 384, 160, 32, 32, False)
+        enemy1 = Player(550, 200, 72, 72, "../assets/images/zombie2.png", 48*4, 0, 48, 48, False)
         enemy1.change_hp(100)
         enemy_party.members += [enemy1]
 
-        enemy_image = pygame.image.load("../assets/images/OtherSheet.png").convert_alpha()
-        enemy_rect = pygame.Rect(64, 128, 32, 32)
+        enemy_image = pygame.image.load("../assets/images/zombie_magic.png").convert_alpha()
+        enemy_rect = pygame.Rect(48, 0, 48, 48)
         enemy_image = enemy_image.subsurface(enemy_rect).copy()
         enemy_party2 = Party((320, 400),
-                            32,
-                            (32, 32),
+                            48,
+                            (48, 48),
                             enemy_image,
                             campaign.world,
                             battle_listener)
-        enemy1 = Player(550, 200, 68, 98, "../assets/images/OtherSheet.png", 64, 128, 32, 32, False)
-        enemy2 = Player(550, 200, 68, 98, "../assets/images/OtherSheet.png", 64, 128, 32, 32, False)
-        enemy3 = Player(550, 200, 68, 98, "../assets/images/OtherSheet.png", 64, 128, 32, 32, False)
+        enemy1 = Player(550, 200, 72, 72, "../assets/images/zombie_magic.png", 48*4, 0, 48, 48, False)
+        enemy2 = Player(550, 200, 72, 72, "../assets/images/zombie2.png", 48*4, 0, 48, 48, False)
+        enemy3 = Player(550, 200, 72, 72, "../assets/images/zombie1.png", 48*4, 0, 48, 48, False)
         enemies = [enemy1, enemy2, enemy3]
         for e in enemies:
             e.change_hp(-35)
